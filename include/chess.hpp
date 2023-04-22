aeb6cede650708bcc57f5f931f07d20671c96678
Yao-Chung <yaochung9744@gmail.com>
2023-04-21 22:51:17 -0700

    Move enum to public

--- include/chess.hpp
+++ include/chess.hpp
@@ -2,9 +2,9 @@
 #define CHESS_GUARD
 
 class Chess {
+    public:
         enum class ChessColor {RED = 0, BLACK = 1};
         enum class ChessID {SOLDIER = 0, CANNON, HOURSE, CAR, ELEPHANT, GUARD, KING};
-    public:
         Chess(unsigned row, unsigned col, ChessColor color, ChessID id);
         void move(unsigned nextRow, unsigned nextCol);
         unsigned row, col;
