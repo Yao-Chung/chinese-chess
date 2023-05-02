62c802a3af6413eefb755342df92f9e0198e9495
Yao-Chung <yaochung9744@gmail.com>
2023-05-02 00:28:30 -0700

    add unfold method in chess

--- include/chess.hpp
+++ include/chess.hpp
@@ -5,9 +5,11 @@ class Chess {
     public:
         enum class ChessColor {RED = 0, BLACK = 1};
         enum class ChessID {SOLDIER = 0, CANNON, HOURSE, CAR, ELEPHANT, GUARD, KING};
-        Chess(unsigned row, unsigned col, ChessColor color, ChessID id);
+        Chess(unsigned row, unsigned col, ChessColor color, ChessID id, bool covered);
         void move(unsigned nextRow, unsigned nextCol);
+        void unfold();
         unsigned row, col;
+        bool covered;
         ChessColor color;
         ChessID id;
 };
--- lib/chess.cpp
+++ lib/chess.cpp
@@ -1,9 +1,13 @@
 #include <cstdlib>
 #include <chess.hpp>
 
-Chess::Chess(unsigned row, unsigned col, ChessColor color, ChessID id) : row(row), col(col), color(color), id(id) {}
+Chess::Chess(unsigned row, unsigned col, ChessColor color, ChessID id, bool covered) : row(row), col(col), color(color), id(id), covered(covered){}
 
 void Chess::move(unsigned nextRow, unsigned nextCol) {
-    this->row = nextRow;
-    this->col = nextCol;
+    row = nextRow;
+    col = nextCol;
+}
+
+void Chess::unfold() {
+    covered = false;
 }
