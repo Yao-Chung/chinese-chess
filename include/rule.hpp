033063dc3d8fedd4e24f75c1779d79c765deed26
Yao-Chung <yaochung9744@gmail.com>
2023-05-03 00:43:34 -0700

    add validate result

--- include/rule.hpp
+++ include/rule.hpp
@@ -8,20 +8,23 @@ class Rule {
         Board *board;
     public:
         Rule(Board *board);
-        enum class RuleReturnType {VALID = 0, INVALID, WIN, LOSE};
+        enum class RuleReturnType {VALID_MOVE = 0, VALID_UNFOLD, VALID_EAT, INVALID, RED_WIN, BLACK_WIN, CONTINUE};
         virtual RuleReturnType validateMove(Chess &chess, unsigned nextRow, unsigned nextCol) = 0;
+        virtual RuleReturnType validateResult() = 0;
 };
 
 class BigBoardRule : public Rule {
     public:
         BigBoardRule(Board *board);
         RuleReturnType validateMove(Chess &chess, unsigned nextRow, unsigned nextCol) override;
+        RuleReturnType validateResult() override;
 };
 
 class SmallBoardRule : public Rule {
     public:
         SmallBoardRule(Board *board);
         RuleReturnType validateMove(Chess &chess, unsigned nextRow, unsigned nextCol) override;
+        RuleReturnType validateResult() override;
         RuleReturnType validateUnfold(Chess &chess);
 };
 
--- lib/rule.cpp
+++ lib/rule.cpp
@@ -1,9 +1,82 @@
 #include <rule.hpp>
+#include <chess.hpp>
+#include <cstdlib>
 
-Rule::RuleReturnType BigBoardRule::validate(Chess chess, unsigned nextRow, unsigned nextCol) {
-    return true;
+Rule::Rule(Board *board) : board(board) {}
+
+BigBoardRule::BigBoardRule(Board *board) : Rule(board) {}
+
+SmallBoardRule::SmallBoardRule(Board *board) : Rule(board) {}
+
+Rule::RuleReturnType BigBoardRule::validateMove(Chess &chess, unsigned nextRow, unsigned nextCol) {
+    return RuleReturnType::VALID_MOVE;
 }
 
-Rule::RuleReturnType SmallBoardRule::validate(Chess chess, unsigned nextRow, unsigned nextCol) {
-    return true;
+Rule::RuleReturnType SmallBoardRule::validateMove(Chess &chess, unsigned nextRow, unsigned nextCol) {
+    using ChessID = Chess::ChessID;
+
+    // Check the boundary of board and if it's covered
+    if(nextRow < 0 || nextRow >= board->rows || nextCol < 0 || nextCol >= board->cols || chess.folded) {
+        return RuleReturnType::INVALID;
+    }
+    // Check the next position is near
+    if(std::abs((int)nextRow-(int)chess.row) + std::abs((int)nextCol-(int)chess.col) != 1 && chess.id != ChessID::CANNON) {
+        return RuleReturnType::INVALID;
+    }
+    if(!board->contains({nextRow, nextCol})) {
+        return RuleReturnType::VALID_MOVE;
+    }else {
+         // Has another chess
+        Chess nextChess = board->at({nextRow, nextCol});
+        if(nextChess.color == chess.color) {
+            return RuleReturnType::INVALID;
+        }
+        if(chess.id == ChessID::CANNON) {
+            // Check if next position is in the same row / col
+            if(nextRow != chess.row && nextCol != chess.col) {
+                return RuleReturnType::INVALID;
+            }
+            unsigned minRow = std::min(nextRow, chess.row), minCol = std::min(nextCol, chess.col);
+            unsigned maxRow = std::max(nextRow, chess.row), maxCol = std::max(nextCol, chess.col);
+            unsigned cnt = 0; 
+            for(unsigned i=minRow+1; i<=maxRow-1; i++) {
+                cnt += (board->contains({i, chess.col}));
+            }
+            for(unsigned j=minCol+1; j<=maxCol-1; j++) {
+                cnt += (board->contains({chess.row, j}));
+            }
+            return (cnt == 1) ? RuleReturnType::VALID_EAT : RuleReturnType::INVALID;
+        }else if(chess.id == ChessID::SOLDIER) {
+            return (nextChess.id == ChessID::KING) ? RuleReturnType::VALID_EAT : RuleReturnType::INVALID;
+        }else if(chess.id == ChessID::KING) {
+            return (nextChess.id != ChessID::SOLDIER) ? RuleReturnType::VALID_EAT : RuleReturnType::INVALID;
+        }else {
+            return (chess.id >= nextChess.id) ? RuleReturnType::VALID_EAT : RuleReturnType::INVALID;
+        }
+    }
+}
+
+Rule::RuleReturnType SmallBoardRule::validateUnfold(Chess &chess) {
+    if(chess.folded == false) {
+        return RuleReturnType::INVALID;
+    }
+    return RuleReturnType::VALID_UNFOLD;
+}
+
+Rule::RuleReturnType SmallBoardRule::validateResult() {
+    unsigned blackN = 0, redN = 0;
+    for(auto [_, chess] : *board) {
+        if(chess.color == Chess::ChessColor::BLACK) {
+            blackN += 1;
+        }else {
+            redN += 1;
+        }
+    }
+    if(redN == 0) {
+        return RuleReturnType::BLACK_WIN;
+    }else if(blackN == 0) {
+        return RuleReturnType::RED_WIN;
+    }else {
+        return RuleReturnType::CONTINUE;
+    }
 }
