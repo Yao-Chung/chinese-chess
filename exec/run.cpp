854ba5ef251063227ce7a06c090b35872e4848de
Yao-Chung <yaochung9744@gmail.com>
2023-05-07 17:49:49 -0700

    Add SFML and test code

--- exec/run.cpp
+++ exec/run.cpp
@@ -1,7 +1,21 @@
-#include <map>
-#include <iostream>
-#include <board.hpp>
+#include <SFML/Graphics.hpp>
 
-int main(int argc, char* const argv[]) {
-    return 0;
+int main()
+{
+    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
+    window.setFramerateLimit(144);
+
+    while (window.isOpen())
+    {
+        for (auto event = sf::Event{}; window.pollEvent(event);)
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.display();
+    }
 }
